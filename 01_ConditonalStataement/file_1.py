ef9c11af245570638af7c2fd08d92b7a8f5d047c
Ivan Gospodinov <ivan9069@gmail.com>
2025-08-16 20:16:20 +0300

    Update file_1.py

--- 01_ConditonalStataement/file_1.py
+++ 01_ConditonalStataement/file_1.py
@@ -1,6 +1,6 @@
 rows = int(input("Enter your number:"))
 
-# "this is first pattern test in python
+# this is first pattern test in python
 
 for i in range(0, rows - 1):
     for j in range(0, rows -i - 1):
