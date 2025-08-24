7f6bf1b0d3e9170618dd1a0a87968f4d2bdce971
Ivan Gospodinov <ivan9069@gmail.com>
2025-08-24 18:09:51 +0300

    Create 06_string_pureness.py
    add new file

---
+++ 02_Conditional Statements, and Loops/06_string_pureness.py
@@ -0,0 +1,15 @@
+number_of_string = int(input())
+
+is_true = True
+
+for _ in range(number_of_string):
+    word = input()
+    for index, char in enumerate(word):
+        if char == ',' or char == '.' or char == '_':
+            is_true = False
+            print(f"{word}, is not pure!")
+            break
+
+    if is_true:
+        print(f"{word}, is pure.")
+
