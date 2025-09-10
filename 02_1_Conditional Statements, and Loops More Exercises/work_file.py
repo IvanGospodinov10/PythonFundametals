5cc8ed4fae920609e834fa5ec0e87de75b4a068b
Ivan Gospodinov <ivan9069@gmail.com>
2025-09-10 21:49:17 +0300

    update 01_number_definer.py

--- 02_1_Conditional Statements, and Loops More Exercises/work_file.py
+++ 02_1_Conditional Statements, and Loops More Exercises/work_file.py
@@ -1,2 +1,10 @@
-print("555" * 3)
-print([5] * 3)
+# print("555" * 3)
+# print([5] * 3)
+
+# test_list_a = [1,2,3,4,5,6]
+test_list_a = ['a','b','c','d','i','f']
+
+
+# for index in test_list_a:
+#     print(index)
+print(test_list_a[2])
