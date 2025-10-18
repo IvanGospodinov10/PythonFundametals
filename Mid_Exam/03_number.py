9fe5732bd8f4bce69431b91a3d7e5a2c37768e5f
Ivan Gospodinov <ivan9069@gmail.com>
2025-10-18 20:39:02 +0300

    Delete 03_number.py

--- Mid_Exam/03_number.py
+++
@@ -1,8 +0,0 @@
-numbers = list(map(int, input().split()))
-
-average_number = sum(numbers) / len(numbers)
-filtered_numbers = [number for number in numbers if number > average_number]
-if filtered_numbers == []:
-    print("No")
-else:
-    print(" ".join(str(num) for num in sorted(filtered_numbers, reverse=True)[:5]))
