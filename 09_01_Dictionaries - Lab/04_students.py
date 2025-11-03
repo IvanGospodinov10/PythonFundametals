89ef8b808f7c9acf98f0a1515b9d55523ee495b0
Ivan Gospodinov <ivan9069@gmail.com>
2025-11-03 21:27:07 +0200

    add file 04_students.py

---
+++ 09_01_Dictionaries - Lab/04_students.py
@@ -0,0 +1,19 @@
+student_dict = {}
+
+command = input()
+while ":" in command:
+    info = command.split(":")
+
+    student_name, student_id, student_course = info[0], info[1], info[2]
+    if student_course not in student_dict:
+        student_dict[student_course] = {}
+    student_dict[student_course][student_id] = student_name
+    # print(student_dict)
+    command = input()
+
+course = " ".join(command.split("_"))
+for key,value in student_dict.items():
+    if key == course:
+        for student_id, student_name in value.items():
+            print(f"{student_name} - {student_id}")
+
