95cf1da8f05bd0b87497584e09943bc323fa07ec
Ivan Gospodinov <ivan9069@gmail.com>
2025-11-22 07:33:39 +0200

    01_valid_usernames.py

--- 10_02_Text Processing - Exercise/01_valid_usernames.py
+++ 10_02_Text Processing - Exercise/01_valid_usernames.py
@@ -1,7 +1,7 @@
 text = input().split(", ")
 for username in text:
     username_is_valid = True
-    if not (3 < len(username) <= 16):
+    if not (3 <= len(username) <= 16):
         username_is_valid = False
     for symbol in username:
         if not (symbol.isalnum() or symbol == "-" or symbol == "_"):
